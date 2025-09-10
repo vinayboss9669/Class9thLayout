cf7164d737faade937baa17ca6bda3b6ef2d965b
Vinaykumar96 <vinayboss9669@gmail.com>
2025-09-10 23:33:35 +0530

    Add Subject schema definition

---
+++ Entities/Subject.js
@@ -0,0 +1,27 @@
+{
+  "name": "Subject",
+  "type": "object",
+  "properties": {
+    "name": {
+      "type": "string",
+      "description": "Subject name"
+    },
+    "icon": {
+      "type": "string",
+      "description": "Icon name for the subject"
+    },
+    "color": {
+      "type": "string",
+      "description": "Color theme for the subject"
+    },
+    "description": {
+      "type": "string",
+      "description": "Brief description of the subject"
+    }
+  },
+  "required": [
+    "name",
+    "icon",
+    "color"
+  ]
+}
